0a9350eb4aaaf156045689a3ae6d7ae7fff4ea12
dansc00 <danielsousa.cipriano@gmail.com>
2025-12-23 03:41:27 -0300

    removing tutorial bash script

--- python_venv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# install requirements and dependencies
-pip3 install -r requirements.txt
-pip3 install -e .
-
-# python3 -m venv .venv to install
-# source .venv/bin/activate to start
-# deactivate to finish
